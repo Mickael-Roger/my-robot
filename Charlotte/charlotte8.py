f0b784f6ea1046e960b2bf690d8d2a1de4695b5e
Mickael-Roger <33000645+Mickael-Roger@users.noreply.github.com>
2019-11-03 19:31:46 +0100

    Create charlotte8.py

---
+++ Charlotte/charlotte8.py
@@ -0,0 +1,14 @@
+from robotlib import start, avance, recule, droite, gauche, stop, fin, objetdevant, objetderriere, image, img
+
+start()
+
+
+img.capture()
+img.afficher()
+print("NB : ", img.nombredevisage())
+print("Position : ", img.positionvisage())
+
+
+
+
+fin()
